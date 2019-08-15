57561e27be68739c813972b499da842256831414
KHorii8 <53989729+KHorii8@users.noreply.github.com>
2019-08-15 23:09:29 +0900

    study0815

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,107 +1,196 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h1>チュートリアルToDoリスト</h1>
+
+    <label v-for="label in options">
+      <input type="radio" v-model="current" v-bind:value="label.value">
+      {{ label.label }}
+    </label>
+    （{{ computedTodos.length }} 件を表示）
+    <table>
+      <thead v-pre>
+        <tr>
+          <th class="id">ID</th>
+          <th class="comment">コメント</th>
+          <th class="state">状態</th>
+          <th class="button">-</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- ★STEP5 ToDo の要素をループ -->
+        <tr v-for="item in computedTodos" v-bind:key="item.id" v-bind:class="{done:item.state}">
+          <th>{{ item.id }}</th>
+          <td>{{ item.comment }}</td>
+          <td class="state">
+            <!-- ★STEP10 状態変更ボタン -->
+            <button v-on:click="doChangeState(item)">{{ labels[item.state] }}</button>
+          </td>
+          <td class="button">
+            <button v-on:click.ctrl="doRemove(item)">削除</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p>※削除ボタンはコントロールキーを押しながらクリックして下さい</p>
+
+    <!-- ★STEP6 -->
+    <h2>新しい作業の追加</h2>
+    <form class="add-form" v-on:submit.prevent="doAdd">
+      <!-- コメント入力フォーム -->
+      コメント
+      <input type="text" ref="comment">
+      <!-- 追加ボタンのモック -->
+      <button type="submit">追加</button>
+    </form>
   </div>
 </template>
 
 <script>
+// ここは外だししたい
+var STORAGE_KEY = "todos-vuejs-demo";
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+
+  data() {
+    return {
+      todos: [],
+      current: -1,
+      options: [
+        { id: 1, value: -1, label: "すべて" },
+        { id: 2, value: 0, label: "作業中" },
+        { id: 3, value: 1, label: "完了" }
+      ]
+    };
+  },
+
+  computed: {
+    computedTodos() {
+      return this.todos.filter(function(el) {
+        return this.current < 0 ? true : this.current === el.state;
+      }, this);
+    },
+
+    // ★STEP13 作業中・完了のラベルを表示する
+    labels() {
+      return this.options.reduce(function(a, b) {
+        return Object.assign(a, { [b.value]: b.label });
+      }, {});
+      // キーから見つけやすいように、次のように加工したデータを作成
+      // {0: '作業中', 1: '完了', -1: 'すべて'}
+    }
+  },
+
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
+  },
+
+  created() {
+    this.todos = todoStorage.fetch();
+  },
+
+  methods: {
+    doAdd(event, value) {
+      let comment = this.$refs.comment;
+      if (!comment.value.length) {
+        return;
+      }
+
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      });
+
+      comment.value = "";
+    },
+
+    doChangeState(item) {
+      item.state = !item.state ? 1 : 0;
+    },
+
+    doRemove(item) {
+      let index = this.todos.indexOf(item);
+      this.todos.splice(index, 1);
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+* {
+  box-sizing: border-box;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#app {
+  max-width: 640px;
+  margin: 0 auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+table {
+  width: 100%;
+  border-collapse: collapse;
 }
-a {
-  color: #42b983;
+thead th {
+  border-bottom: 2px solid #0099e4; /*#d31c4a */
+  color: #0099e4;
+}
+th,
+th {
+  padding: 0 8px;
+  line-height: 40px;
+}
+thead th.id {
+  width: 50px;
+}
+thead th.state {
+  width: 100px;
+}
+thead th.button {
+  width: 60px;
+}
+tbody td.button,
+tbody td.state {
+  text-align: center;
+}
+tbody tr td,
+tbody tr th {
+  border-bottom: 1px solid #ccc;
+  transition: all 0.4s;
+}
+tbody tr.done td,
+tbody tr.done th {
+  background: #f8f8f8;
+  color: #bbb;
+}
+tbody tr:hover td,
+tbody tr:hover th {
+  background: #f4fbff;
+}
+button {
+  border: none;
+  border-radius: 20px;
+  line-height: 24px;
+  padding: 0 8px;
+  background: #0099e4;
+  color: #fff;
+  cursor: pointer;
 }
 </style>
